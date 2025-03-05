a770561e25a652b9e8e936cc7b82c17db001ccc1
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-06 01:18:35 +0800

    Add brightness configuration to light theme in AppTheme class

--- lib/core/config/theme/app_theme.dart
+++ lib/core/config/theme/app_theme.dart
@@ -5,6 +5,7 @@ class AppTheme {
   static final lightTheme = ThemeData(
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.lightBackground,
+    brightness: Brightness.light,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
