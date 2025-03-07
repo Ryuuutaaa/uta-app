13d25a80b2badd9f960ecef033237e7635600881
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-08 01:42:52 +0800

    Convert SplashPage from StatelessWidget to StatefulWidget for enhanced state management

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:uta_app/core/configs/assets/app_vectors.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
