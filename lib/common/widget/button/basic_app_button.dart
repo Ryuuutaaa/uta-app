3a9545200beb425f946c378b4b401082fae28f87
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-08 14:24:31 +0800

    Implement BasicAppButton to return an ElevatedButton with onPressed and child properties for enhanced functionality

--- lib/common/widget/button/basic_app_button.dart
+++ lib/common/widget/button/basic_app_button.dart
@@ -7,6 +7,9 @@ class BasicAppButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: child,
+    );
   }
 }
