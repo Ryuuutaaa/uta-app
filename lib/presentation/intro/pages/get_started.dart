a966622272440a7f58195d21bd534058576e68fc
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-08 14:04:52 +0800

    Add SVG logo to GetStartedPage and enhance layout with a semi-transparent overlay

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:uta_app/core/configs/assets/app_images.dart';
+import 'package:uta_app/core/configs/assets/app_vectors.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -16,7 +18,18 @@ class GetStartedPage extends StatelessWidget {
                 image: AssetImage(AppImages.introBG),
               ),
             ),
-          )
+            child: Column(
+              children: [
+                SvgPicture.asset(
+                  AppVectors.logo,
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
         ],
       ),
     );
