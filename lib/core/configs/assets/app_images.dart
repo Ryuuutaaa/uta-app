e48fa32cf2edf726e6f15f4966de8b228cd25df8
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-10 01:02:13 +0800

    Add chooseModeBG image path to AppImages for ChooseMode page

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -3,4 +3,5 @@ class AppImages {
   static const String format = ".png";
 
   static const String introBG = "${basePath}intro_bg$format";
+  static const String chooseModeBG = "${basePath}choose_mode_bg$format";
 }
