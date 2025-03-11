3bdf4ad3a7480d1ff1898b005af1c4a2f540800c
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-11 11:55:14 +0800

    Initialize HydratedBloc storage in main function for state persistence

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:uta_app/core/configs/theme/app_theme.dart';
 import 'package:uta_app/presentation/splash/pages/splash.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb ? HydratedStorageDirectory.web : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
   runApp(const MyApp());
 }
 
