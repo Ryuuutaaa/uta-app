0d5c02c44ecb935aa1e55d9b93708ab76cdf8ade
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-11 12:03:00 +0800

    Integrate ThemeCubit with MultiBlocProvider for theme management in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:uta_app/core/configs/theme/app_theme.dart';
+import 'package:uta_app/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:uta_app/presentation/splash/pages/splash.dart';
 
 Future<void> main() async {
@@ -18,10 +20,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      home: const SplashPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => ThemeCubit(),
+        ),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, mode) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme,
+          home: const SplashPage(),
+        ),
+      ),
     );
   }
 }
